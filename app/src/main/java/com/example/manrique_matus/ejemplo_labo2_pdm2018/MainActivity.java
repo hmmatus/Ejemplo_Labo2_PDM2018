3f8182687d8db814ad9584cb8cbea96c154da3ce
Hector Manrique Matus Henriquez <manriquematus1@gmail.com>
2018-03-04 22:52:47 -0600

    Second push

--- app/src/main/java/com/example/manrique_matus/ejemplo_labo2_pdm2018/MainActivity.java
+++ app/src/main/java/com/example/manrique_matus/ejemplo_labo2_pdm2018/MainActivity.java
@@ -8,25 +8,39 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn;
-    TextView text;
-    EditText edit;
+    //Creamos las variables
+    private Button btn;
+    private TextView text;
+    private TextView result;
+    private EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Esto configura el titulo que aparece en la barra de herramientas
         getSupportActionBar().setTitle("Ejemplo RelativeLayout");
 
+        //Enlazamos la variable al campo dentro del xml conectado al onCreate
         btn=(Button)findViewById(R.id.button_test);
         text=(TextView) findViewById(R.id.text);
+        result=(TextView)findViewById(R.id.text2);
         edit=(EditText) findViewById(R.id.editT);
 
+        //El boton realiza una accion al darle tab que es el onClick
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                text.setText(edit.getText());
+                text.setVisibility(View.VISIBLE);
+                result.setText(edit.getText());
             }
         });
+    }
+    //Otra manera de enlazar la funcion a un xml es agregandole el atributo onClick con el nombre de la funcion
+    public void reset(View view){
+        text.setVisibility(View.INVISIBLE);
+        result.setText("");
+
     }
 }
